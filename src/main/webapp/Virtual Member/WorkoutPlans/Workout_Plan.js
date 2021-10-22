1bd4ffdddddc77acb2a1ba3d3dfb425e23c6bcdc
RangikaShalani <rangika2520@gmail.com>
2021-10-22 12:15:57 +0530

    remove virtualM

--- src/main/webapp/Virtual Member/WorkoutPlans/Workout_Plan.js
+++
@@ -1,8 +0,0 @@
-function virtual_workout_plan(){
-    let anchor_workout = document.getElementById("phy_mem_workout");
-    let anchor_workout_i = document.getElementById("phy_mem_workout_i");
-    let workout_text = document.getElementById("physical_member_workout_text");
-    anchor_workout.style.backgroundColor = "white";
-    anchor_workout_i.style.color = "black";
-    workout_text.style.color = "black";
-}
